1d5253c68779654ab3f01f656955e82e8fba881c
Zeddy <zeddyemy@gmail.com>
2023-08-07 22:02:07 +0100

    docs: add detailed info as comments to frontpage template part

--- template-parts/frontpage.php
+++ template-parts/frontpage.php
@@ -4,7 +4,12 @@
     Description : This Template Part Is Made Specifically For The Front Page.
     It Also Consists Of Other Template Parts That Brings About The Design And Looks Of The FrontPage.
 
-    Template Parts :    
+    Template Parts :    1) home section template part
+                        2) about section template part
+                        3) goal section template part
+                        4) service section template part
+                        5) blog section template part
+                        6) portfolio section template part
 
 */
 
