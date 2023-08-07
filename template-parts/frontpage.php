e7da1229a460f824a65f80dcb1e6e4330af778a2
Zeddy <zeddyemy@gmail.com>
2023-08-07 21:36:58 +0100

    feat: add frontpage template part

---
+++ template-parts/frontpage.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    Template part : Front Page
+    Description : This Template Part Is Made Specifically For The Front Page.
+    It Also Consists Of Other Template Parts That Brings About The Design And Looks Of The FrontPage.
+
+    Template Parts :    
+
+*/
+
+?>
+
+<section class="wrapper col-12">
+    Front Page
+</section>
