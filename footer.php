718780632bbad6250bb1b0b809bb302dcdbad015
Zeddy <zeddyemy@gmail.com>
2023-08-07 21:21:26 +0100

    feat: Add footer section to the theme

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * MTS footer
+ *
+ * @package MTS
+ */
+
+?>
+<footer class="footer col-12">
+    <div class="footer-b flex flexCenter">
+        <p> © 2022 Meridian Tech | All Right Reserved. </p>
+    </div>
+</footer>
+
+
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
