7f8af07d4a3c25bb705a93c7732f75862b1e7e3d
Zeddy <zeddyemy@gmail.com>
2023-08-09 23:42:42 +0100

    feat: add conditins to include 'about section' and 'goal section' on the frontpag template part

--- template-parts/frontpage.php
+++ template-parts/frontpage.php
@@ -1,5 +1,5 @@
 <?php
-/*
+    /*
     Template part : Front Page
     Description : This Template Part Is Made Specifically For The Front Page.
     It Also Consists Of Other Template Parts That Brings About The Design And Looks Of The FrontPage.
@@ -13,14 +13,20 @@
 
 */
 
+    $toggle_about_sec  = get_theme_mod('toggle_about_sec');
+    $toggle_goal_sec  = get_theme_mod('toggle_goal_sec');
 ?>
 
 <section class="wrapper col-12">
     <?php
     get_template_part('template-parts/frontpage-parts/home-section', get_post_format());
 
-    get_template_part('template-parts/frontpage-parts/about-section', get_post_format());
-    get_template_part('template-parts/frontpage-parts/goal-section', get_post_format());
+    if ($toggle_about_sec == true) {
+        get_template_part('template-parts/frontpage-parts/about-section', get_post_format());
+    }
+    if ($toggle_goal_sec == true) {
+        get_template_part('template-parts/frontpage-parts/goal-section', get_post_format());
+    }
     get_template_part('template-parts/frontpage-parts/services-section', get_post_format());
     get_template_part('template-parts/frontpage-parts/blog-section', get_post_format());
     get_template_part('template-parts/frontpage-parts/portfolio-section', get_post_format());
