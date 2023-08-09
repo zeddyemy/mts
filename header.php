d64bc3880416ce97b1020a9300ee93b74186aabd
Zeddy <zeddyemy@gmail.com>
2023-08-09 18:20:44 +0100

    chore: remove hard coded css links

--- header.php
+++ header.php
@@ -17,17 +17,6 @@
 
     <?php wp_head(); ?>
     <title>Meridian Tech Solutions</title>
-
-    <link rel="stylesheet" href="./assets/css/home.css">
-    <link rel="stylesheet" href="./assets/css/header.css">
-    <link rel="stylesheet" href="./assets/css/services.css">
-    <link rel="stylesheet" href="./assets/css/goal.css">
-    <link rel="stylesheet" href="./assets/css/blog.css">
-    <link rel="stylesheet" href="./assets/css/about.css">
-    <link rel="stylesheet" href="./assets/css/main.css">
-    <link rel="stylesheet" href="./assets/library/boxicons/css/boxicons.min.css">
-    <link rel="stylesheet" href="./assets/library/tiny-slider-master/dist/tiny-slider.css?ver=6.0.2">
-    <link rel="stylesheet" href="./assets/library/aos/aos.css">
 </head>
 
 <body <?php body_class(''); ?>>
