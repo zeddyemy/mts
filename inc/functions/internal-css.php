26328ea6f5213c8427fb7ddc722d8cba3cf9e9dc
Zeddy <zeddyemy@gmail.com>
2023-08-10 00:42:31 +0100

    chore: replace css variable '--theme-color' with '--theme-clr' across all files

--- inc/functions/internal-css.php
+++ inc/functions/internal-css.php
@@ -1,6 +1,7 @@
 <?php
 
-function internal_css() {
+function internal_css()
+{
 
     $bgColor = get_theme_mod('background_color', get_theme_support('custom-background', 'default-color')); // Custom background Color
     $themeColor = get_theme_mod('mts_theme_color', '#2869ff'); // Theme Color
@@ -17,11 +18,11 @@ function internal_css() {
     $titleCaseMod  = get_theme_mod('title_text_transform', 'capitalize');
     $titleCase = ($titleCaseMod == 'capitalize') ? 'capitalize' : (($titleCaseMod == 'uppercase') ? 'uppercase' : 'lowercase');
 
-    $adminBar = is_admin_bar_showing();?>
+    $adminBar = is_admin_bar_showing(); ?>
 
     <style>
         body {
-            --theme-color: <?php echo $themeColor;  ?>;
+            --theme-clr: <?php echo $themeColor;  ?>;
             --header-text-color: <?php echo $headerTextColor;  ?>;
             --header-background-color: <?php echo $headerBgColor;  ?>;
             --navbar-background-color: <?php echo $headerBgColor . 'c5'; ?>;
@@ -33,14 +34,14 @@ function internal_css() {
             --title-text-transform: <?php echo $navbarCase; ?>;
             --navbar-text-transform: <?php echo $titleCase; ?>;
         }
-        
+
         <?php
-            // if custom background color is not set
-            if (!$bgColor) {
-                echo 'body { background-image: var(--body-background); }';
-            }
+        // if custom background color is not set
+        if (!$bgColor) {
+            echo 'body { background-image: var(--body-background); }';
+        }
         ?>
     </style> <?php
 
-}
-add_action('wp_head', 'internal_css');
+            }
+            add_action('wp_head', 'internal_css');
