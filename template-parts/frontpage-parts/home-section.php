fbcddf696e52a1b983eb0c4d79332a3c9580dd9d
Zeddy <zeddyemy@gmail.com>
2023-08-21 01:16:30 +0100

    feat: add content to the 'home section' template part with CSS styling
    Added HTML code for the home section, completing the
    front page layout. CSS styles for the home section have been refined
    and organized in 'home.css' to enhance the visual presentation.

--- template-parts/frontpage-parts/home-section.php
+++ template-parts/frontpage-parts/home-section.php
@@ -1,3 +1,20 @@
 <?php
 
 global $mtsThemeMods;
+?>
+
+<section id="home" class="home noPad fitImg" style="background-image: url('<?php echo get_mts_assets('img') . 'team.jpg' ?>');">
+    <div class="container col-12">
+        <div class="slide flex flexCenter">
+            <div class="home-content flex flexCenter" data-aos="fade-up" data-aos-easing="ease-in-out-quart">
+                <h1> Stay ahead of the curve with our forward thinking. </h1>
+                <?php mts_render_btn('Get in touch', '#'); ?>
+            </div>
+        </div>
+    </div>
+    <a href="#about" class="mouse smoothscroll" data-scrollto="about">
+        <span class="mouse-icon">
+            <span class="mouse-wheel"></span>
+        </span>
+    </a>
+</section>
