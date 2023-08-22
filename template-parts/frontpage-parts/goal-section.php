4c7c77ec7318f55bc13ddb6f5b237712bd8053e7
Zeddy <zeddyemy@gmail.com>
2023-08-22 12:39:08 +0100

    chore: Refine styling and structure of the goal section
    Made further improvements to the goal section, refining its CSS styles and
    adjusting its structure to improve its visual presentation and alignment
    with the overall design. These enhancements contribute to a more engaging
    and coherent user experience. The section at the moment also contain
    hard coded tags. In the future it will be made dynamic and optional.

--- template-parts/frontpage-parts/goal-section.php
+++ template-parts/frontpage-parts/goal-section.php
@@ -4,14 +4,28 @@ global $mtsThemeMods;
 
 ?>
 <section id="goal" class="goal noPad">
-    <div class="container col-12 noPad">
-        <div class="col-6 noPad">
-            <div class="fitImg imgBg" style="background-image: url('<?php echo $mtsThemeMods['goal_sec_img']; ?>')"></div>
-        </div>
-        <div class="col-6 noPad" data-aos="fade-up" data-aos-easing="ease-in-out-quart">
-            <div class="txtContain">
-                <span class="title"> <?php echo $mtsThemeMods['goal_sec_title']; ?> </span>
-                <p> <?php echo $mtsThemeMods['goal_sec_content']; ?> </p>
+    <div class="container col-12">
+        <div class="card secCard flex flexCenter row col-12 fitImg" data-aos="fade-up" data-aos-easing="ease-in-out-quart" style="background-image: url('<?php echo $mtsThemeMods['goal_sec_img']; ?>')">
+            <div class="col-8 noPad" data-aos="fade-up" data-aos-easing="ease-in-out-quart">
+                <div class="cardBody">
+                    <span class="title"> <?php echo $mtsThemeMods['goal_sec_title']; ?> </span>
+                    <p> <?php echo $mtsThemeMods['goal_sec_content']; ?> </p>
+                </div>
+            </div>
+            <div class="col-4 noPad" data-aos="fade-up" data-aos-easing="ease-in-out-quart">
+                <div class="cardBody">
+                    <div class="flex goalTags">
+                        <div class="">
+                            <span>Insightfull</span>
+                        </div>
+                        <div class="">
+                            <span>Fast</span>
+                        </div>
+                        <div class="">
+                            <span>Captivating</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
