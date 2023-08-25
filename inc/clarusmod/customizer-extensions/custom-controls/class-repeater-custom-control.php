c1dba1d7aabf03d48cc0bd94f570ab0e44d6132e
Zeddy <zeddyemy@gmail.com>
2023-08-25 05:32:43 +0100

    chore: update Clarusmod

--- inc/clarusmod/customizer-extensions/custom-controls/class-repeater-custom-control.php
+++ inc/clarusmod/customizer-extensions/custom-controls/class-repeater-custom-control.php
@@ -5,11 +5,12 @@ class Clarusmod_Repeater_Custom_Control extends Clarusmod_Customize_Control
 
     // The type of control being rendered
     public $type = 'repeater';
+    public $default_values = array();
     public $button_labels = array(); // Button labels
     public $input_type = 'text'; // Input Type
     public $placeholder = 'new'; // Input Placeholder
     public $is_sortable = false; // sortable flag
-    
+
     // Constructor
     public function __construct($manager, $id, $args = array(), $options = array())
     {
@@ -21,6 +22,11 @@ class Clarusmod_Repeater_Custom_Control extends Clarusmod_Customize_Control
                 'add' => __('Add', 'clarusmod'),
             )
         );
+
+        // set default values for repeater fields
+        if (isset($args['default_values']) && is_array($args['default_values'])) {
+            $this->default_values = $args['default_values'];
+        }
     }
 
 
@@ -34,6 +40,7 @@ class Clarusmod_Repeater_Custom_Control extends Clarusmod_Customize_Control
     // render control
     public function render_content()
     {
+        $default_values = $this->default_values;
         $input_type = isset($this->input_type) ? esc_attr($this->input_type) : '';
         $placeholder = isset($this->placeholder) ? esc_attr($this->placeholder) : '';
         $sortable_class = $this->is_sortable ? 'sortable' : ''; ?>
@@ -45,8 +52,17 @@ class Clarusmod_Repeater_Custom_Control extends Clarusmod_Customize_Control
                 <span class="customize-control-description"><?php echo esc_html($this->description); ?></span>
             <?php } ?>
 
-            <input type="hidden" id="<?php echo esc_attr($this->id); ?>" name="<?php echo esc_attr($this->id); ?>" value="<?php echo esc_attr($this->value()); ?>" class="customize-control-repeater" <?php $this->link(); ?> />
+            <input type="hidden" id="<?php echo esc_attr($this->id); ?>" name="<?php echo esc_attr($this->id); ?>" value="<?php echo esc_attr(implode(',', $default_values)); ?>" class="customize-control-repeater" <?php $this->link(); ?> />
             <div class="theRepeater <?php echo esc_attr($sortable_class); ?>" data-input-type="<?php echo $input_type; ?>">
+                <?php foreach ($default_values as $value) { ?>
+                    <div class="repeater">
+                        <input type="text" value="<?php echo esc_attr($value); ?>" class="repeater-input" placeholder="<?php echo $placeholder; ?>" />
+                        <?php if ($this->is_sortable) : ?>
+                            <span class="dashicons dashicons-sort"></span>
+                        <?php endif; ?>
+                        <a class="customize-control-repeater-delete" href="#"><span class="dashicons dashicons-trash-alt"></span></a>
+                    </div>
+                <?php } ?>
                 <div class="repeater">
                     <input type="text" value="" class="repeater-input" placeholder="<?php echo $placeholder; ?>" />
                     <?php if ($this->is_sortable) : ?>
@@ -58,5 +74,5 @@ class Clarusmod_Repeater_Custom_Control extends Clarusmod_Customize_Control
             <p class="input-msg">Please type in a valid Url</p>
             <button class="button customize-control-repeater-add" type="button"><?php echo $this->button_labels['add']; ?></button>
         </div>
-    <?php }
+<?php }
 }
