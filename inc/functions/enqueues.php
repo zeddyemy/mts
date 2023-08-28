c2f55eeddbd4a7e484404c7c403d08d7e7343047
Zeddy <zeddyemy@gmail.com>
2023-08-28 12:53:57 +0100

    chore: improve conditionaly statement to enqueue css files
    conditonal statements in enqueue css files was worked upon.
    Four new css files were also created and enqueued:
    -404.css
    - author.css
    - comments.css
    - singles.css
    
    These css files do not have any styles within them yet. Styles will be inlcuded some time in future.
    These many css file are created so that they can be enqueued conditionaly.
    This will help improve perfomance as they will be enqueued only when necessary.

--- inc/functions/enqueues.php
+++ inc/functions/enqueues.php
@@ -31,6 +31,8 @@ if (!is_admin()) {
 
 // Enqueue MTS Styles and Scripts
 function mts_scripts() {
+    global $mtsThemeMods;
+
     // Box Icons.
     wp_enqueue_style('preload-box-icons', get_mts_assets('library/boxicons/css') . 'boxicons.min.css');
 	
@@ -38,28 +40,54 @@ function mts_scripts() {
     wp_enqueue_style('aos', get_mts_assets('library/aos') . 'aos.css');
     wp_enqueue_script('aosJS', get_mts_assets('library/aos') . 'aos.js', '', '1.0', true);
     
-    //tiny slider library
-    wp_enqueue_style('slider', get_mts_assets('library/tiny-slider'). 'tiny-slider.css');
-    wp_enqueue_script('slider-defer', get_mts_assets('library/tiny-slider/min') . 'tiny-slider.js', '', '1.0', true);
-    wp_enqueue_script('tsJSArgs-defer', get_mts_assets('library/tiny-slider') . 'tiny-slider-args.js', '', '1.0', true);
-
     wp_enqueue_style('main', get_mts_assets('css') . 'main.css'); // Main stylesheet
     wp_enqueue_style('header', get_mts_assets('css') . 'header.css'); // Header stylesheet
     wp_enqueue_script('headerJS', get_mts_assets('js') . 'header.js', '', '1.0', true); // Header Script
     wp_enqueue_script('darkmode', get_mts_assets('js') . 'darkmode.js', '', '1.0', true); // Header Script
 
     if (is_front_page() || is_home()) {
-        wp_enqueue_style('home',     get_mts_assets('css') . 'home.css');
-        wp_enqueue_style('mtsAbout', get_mts_assets('css') . 'about.css');
-        wp_enqueue_style('goal',     get_mts_assets('css') . 'goal.css');
-        wp_enqueue_style('blog',     get_mts_assets('css') . 'blog.css');
+        if ($mtsThemeMods['toggle_home_sec'] == true) {
+            wp_enqueue_style('home',     get_mts_assets('css') . 'home.css');
+        }
+        if ($mtsThemeMods['toggle_about_sec'] == true) {
+            wp_enqueue_style('mtsAbout', get_mts_assets('css') . 'about.css');
+        }
+        if ($mtsThemeMods['toggle_goal_sec'] == true) {
+            wp_enqueue_style('goal',     get_mts_assets('css') . 'goal.css');
+        }
+        if ($mtsThemeMods['toggle_services_sec'] == true || is_post_type_archive('services')) {
+            wp_enqueue_style('services', get_mts_assets('css') . 'services.css');
+        }
+        if ($mtsThemeMods['toggle_portfolios_sec'] == true || is_post_type_archive('portfolios')) {
+            wp_enqueue_style('portfolios', get_mts_assets('css') . 'portfolios.css');
+        }
+        if ($mtsThemeMods['toggle_blog_sec'] == true) {
+            wp_enqueue_style('blog', get_mts_assets('css') . 'blog.css');
+        }
     }
 
-    if (is_front_page() || is_home() || is_post_type_archive('services')) {
-        wp_enqueue_style('services', get_mts_assets('css') . 'services.css');
+    if (is_category() || is_search() || is_archive() || is_author()) {
+        wp_enqueue_style('blog', get_mts_assets('css') . 'blog.css');
     }
-    if (is_front_page() || is_home() || is_post_type_archive('portfolios')) {
-        wp_enqueue_style('portfolios', get_mts_assets('css') . 'portfolios.css');
+
+    if (is_single() || is_page()) {
+        // enqueue stylesheet for single post.
+        wp_enqueue_style('single-css', get_mts_assets('css') . 'single.css');
+
+        if (comments_open() || get_comments_number()) {
+            // enqueue stylesheet for the comment section.
+            wp_enqueue_style('comment-css', get_mts_assets('css') . 'comments.css');
+        }
+    }
+
+    if (is_author()) {
+        // enqueue stylesheet for Author page.
+        wp_enqueue_style('author-css', get_mts_assets('css') . 'author.css');
+    }
+
+    if (is_404()) {
+        // enqueue stylesheet for 404 page.
+        wp_enqueue_style('404-css', get_mts_assets('css') . '404.css');
     }
 }
 add_action('wp_enqueue_scripts', 'mts_scripts');
