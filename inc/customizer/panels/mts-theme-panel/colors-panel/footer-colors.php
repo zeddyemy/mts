c6b0e651e808358b603dfff30dca6b173c43bcbe
Zeddy <zeddyemy@gmail.com>
2023-09-20 16:29:35 +0100

    refactor: optimize customizer controls for the footer

--- inc/customizer/panels/mts-theme-panel/colors-panel/footer-colors.php
+++ inc/customizer/panels/mts-theme-panel/colors-panel/footer-colors.php
@@ -14,11 +14,11 @@
 	CUSTOMIZER SETTINGS 
 	+++++++++++++++++++++++++++++++++ */
 
-	// ++ add a setting for footer background color ++ //
+	// add a setting for footer background color //
 	$wp_customize->add_setting(
 		'footer_bg_color', //give it an ID
 		array(
-			'default' => '#212121', // Give it a default
+			'default' => '', // Give it a default
 		)
 	);
 
@@ -26,7 +26,7 @@
 	$wp_customize->add_setting(
 		'footer_text_color', //give it an ID
 		array(
-			'default' => '#f2f2f2', // Give it a default
+			'default' => '', // Give it a default
 		)
 	);
 
@@ -38,36 +38,36 @@
 	CUSTOMIZER CONTROLS FOR THE SETTINGS ABOVE 
 	+++++++++++++++++++++++++++++++++++++++++ */
 
-	// ++ control for footer's background color ++ //
+	// control for footer's background color //
 	$wp_customize->add_control(
 		new WP_Customize_Color_Control(
 			$wp_customize,
-			'footer_bg_color_control', //give it an ID
+			'footer_bg_color',
 			array(
-				'label'      => __( 'Footer Background color', 'mts' ), //set the label to appear in the Customizer
-				'section'    => 'footer_color_sections', //select the section for it to appear under  
-	            'settings'   => 'footer_bg_color' //pick the setting it applies to
+				'label'      => __( 'Footer Background color', 'mts' ),
+				'section'    => 'footer_color_sections',
+				'settings'   => 'footer_bg_color',
 			)
 		)
 	);
 
-	// ++ control for footer's Text color ++ //
+	// control for footer's Text color //
 	$wp_customize->add_control(
 		new WP_Customize_Color_Control(
 			$wp_customize,
-	        'footer_text_color_control', //give it an ID
+			'footer_text_color',
 			array(
-	            'label'      => __( ' Footer Text Color', 'mts' ), //set the label to appear in the Customizer
-				'section'    => 'footer_color_sections', //select the section for it to appear under  
-	            'settings'   => 'footer_text_color' //pick the setting it applies to
+				'label'      => __( ' Footer Text Color', 'mts' ),
+				'section'    => 'footer_color_sections',
+				'settings'   => 'footer_text_color'
 			)
 		)
 	);
 
-	// ++ Control for footer's background image ++ //
+	// Control for footer's background image //
 	$wp_customize->add_control(
 		new WP_Customize_Image_Control(
-			$wp_customize, 'footer_bg_img_control',
+			$wp_customize, 'footer_bg_image',
 		
 			array(
 				'label' => 'Upload Footer Background Image',
