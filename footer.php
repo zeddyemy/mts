9f8ffc183c21fb60571842b018a89e4a1f9d9db8
Emmanuel Zeddy <51033541+zeddyemy@users.noreply.github.com>
2023-09-25 22:19:29 +0100

    dynamically render footer columns and modify portfolios.css and services.css

--- footer.php
+++ footer.php
@@ -6,33 +6,42 @@
  * @package pureFolio
  */
 
+if (is_active_sidebar('footer-widget-one') || is_active_sidebar('footer-widget-two') || is_active_sidebar('footer-widget-three') || is_active_sidebar('footer-widget-four')) {
+    $footerColumns = true;
+} else {
+    $footerColumns = false;
+}
+
 ?>
 <footer class="footer col-12">
-    <div class="footerColumns grid">
-        <?php if (is_active_sidebar('footer-widget-one')) : ?>
-            <div class="column">
-                <?php dynamic_sidebar('footer-widget-one'); ?>
-            </div>
-        <?php endif; ?>
+    <?php if ($footerColumns) : ?>
+        <div class="footerColumns grid">
+            <?php if (is_active_sidebar('footer-widget-one')) : ?>
+                <div class="column">
+                    <?php dynamic_sidebar('footer-widget-one'); ?>
+                </div>
+            <?php endif; ?>
+    
+            <?php if (is_active_sidebar('footer-widget-two')) : ?>
+                <div class="column">
+                    <?php dynamic_sidebar('footer-widget-two'); ?>
+                </div>
+            <?php endif; ?>
+    
+            <?php if (is_active_sidebar('footer-widget-three')) : ?>
+                <div class="column">
+                    <?php dynamic_sidebar('footer-widget-three'); ?>
+                </div>
+            <?php endif; ?>
+    
+            <?php if (is_active_sidebar('footer-widget-four')) : ?>
+                <div class="column">
+                    <?php dynamic_sidebar('footer-widget-four'); ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
 
-        <?php if (is_active_sidebar('footer-widget-two')) : ?>
-            <div class="column">
-                <?php dynamic_sidebar('footer-widget-two'); ?>
-            </div>
-        <?php endif; ?>
-
-        <?php if (is_active_sidebar('footer-widget-three')) : ?>
-            <div class="column">
-                <?php dynamic_sidebar('footer-widget-three'); ?>
-            </div>
-        <?php endif; ?>
-
-        <?php if (is_active_sidebar('footer-widget-four')) : ?>
-            <div class="column">
-                <?php dynamic_sidebar('footer-widget-four'); ?>
-            </div>
-        <?php endif; ?>
-    </div>
     <div class="footer-b flex flexCenter">
         <?php
         echo pureFolio_copyright();
