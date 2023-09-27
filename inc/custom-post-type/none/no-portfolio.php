123b097cdb16258d23dd5ae9ccc8a59cec9621f8
Emmanuel Zeddy <51033541+zeddyemy@users.noreply.github.com>
2023-09-27 12:47:51 +0100

    feat: work on the none-template part as well refactor the functions no_portfolio_post() & no_service_post()

--- inc/custom-post-type/none/no-portfolio.php
+++ inc/custom-post-type/none/no-portfolio.php
@@ -10,11 +10,16 @@ function no_portfolio_post()
 
     <section class="content-none flex flexCenter">
         <div class="card folioCard noneCard" data-aos="zoom-in" data-aos-easing="ease-in-out-quart">
-            <?php if (!$portfolios_query->have_posts()) : ?>
-                <label>NO PORTFOLIOS</label>
-                <span> Oops! Seems you do not have any portfolio yet. Please go to the dashboard to add a new portfolio</span>
-                <?php pureFolio_render_btn('Add New Portfolio', admin_url('post-new.php?post_type=portfolios'), 'plainBtn'); ?>
-            <?php endif; ?>
+            <?php  if (!$portfolios_query->have_posts()) :
+                if (current_user_can('administrator')) : ?>
+                    <label>NO PORTFOLIOS</label>
+                    <span> Oops! Seems you do not have any portfolio yet. Please go to the dashboard to add a new portfolio</span>
+                    <?php pureFolio_render_btn('Add New Portfolio', admin_url('post-new.php?post_type=portfolios'), 'plainBtn');
+                else : ?>
+                    <label> No portfolios </label>
+                    <span>Sorry we currently do not have any portfolio to show, but be rest assured that we'll have something to show you soon.</span>
+                <?php endif;
+            endif; ?>
         </div>
     </section>
 <?php
